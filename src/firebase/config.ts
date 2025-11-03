10d5b50a195af5b95dac886f60b14b94ed327cff
Minal Ali <minalenterprises08@gmail.com>
2025-11-03 14:01:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4693933440-6b942",
+  "appId": "1:179656983837:web:2943c71fe31dc15cf1cdf4",
+  "apiKey": "AIzaSyD8cTqk64vOdJgkQdlNI60AZCgSu5HrpqU",
+  "authDomain": "studio-4693933440-6b942.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "179656983837"
+};
