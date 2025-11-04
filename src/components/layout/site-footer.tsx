26c96eab6ec2d6bb494f56483fd5c3b6a70c1ad6
Minal Ali <minalenterprises08@gmail.com>
2025-11-04 14:38:40 +0000

    my X acc  link https://x.com/MinalCenter

--- src/components/layout/site-footer.tsx
+++ src/components/layout/site-footer.tsx
@@ -44,7 +44,7 @@ export function SiteFooter() {
                   <path d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.64 10 6.7v2.8H7v4h3v9h4v-9z" />
                 </svg>
               </SocialIcon>
-              <SocialIcon href="#">
+              <SocialIcon href="https://x.com/MinalCenter">
                 <svg
                   className="h-5 w-5"
                   fill="currentColor"
