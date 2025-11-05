047776c1fe90070d5055181dd36df3c675bb5723
Minal Ali <minalenterprises08@gmail.com>
2025-11-05 13:13:49 +0000

    make bg fully animated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -90,12 +90,18 @@ export default {
         'infinite-scroll': {
           from: { transform: 'translateX(0)' },
           to: { transform: 'translateX(-100%)' },
-        }
+        },
+        'gradient-bg': {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'infinite-scroll': 'infinite-scroll 40s linear infinite',
+        'gradient-bg': 'gradient-bg 15s ease infinite',
       },
     },
   },
